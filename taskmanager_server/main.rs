24c3a67f6af8377df1718c83fbdc4f3ce0ff38de
Dominik Hoffmann <dominik151099@outlook.de>
2024-03-18 18:11:28 +0100

    Changed memory structure of system_meta

--- taskmanager_server/main.rs
+++ taskmanager_server/main.rs
@@ -1,19 +1,18 @@
 mod rest;
 mod system;
+
 use byte_unit::{Byte, Unit, UnitType};
 use sysinfo::{Components, Disks, Networks, System};
+use system::system_meta::MetaInfo;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    //let mut s = System::new_all();
-    //s.refresh_all();
-    //get_system_meta_info(s)
-    //println!("{} bytes", _s.total_memory());
-
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    // let routes = rest::routes::routes();
-    // println!("Server started at http://localhost:8000");
-    // warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
-    system::system_meta::get_system_meta_info();
+    let routes = rest::routes::routes();
+    println!("Server started at http://localhost:8000");
+    warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
+    let sys_meta: Vec<system::system_meta::MetaInfo> = system::system_meta::get_system_meta_info();
+
+    println!("{:?}", sys_meta)
 }
--- taskmanager_server/system/system_meta.rs
+++ taskmanager_server/system/system_meta.rs
@@ -1,12 +1,7 @@
 extern crate sysinfo;
 
-use std::hash::Hash;
-
-use maplit::hashmap;
-use std::collections::HashMap;
-
-use byte_unit::{Byte, Unit, UnitType};
-use sysinfo::{Components, Disks, Networks, System};
+use byte_unit::{Byte, UnitType};
+use sysinfo::System;
 
 #[derive(Debug)]
 pub struct MetaInfo {
@@ -16,48 +11,86 @@ pub struct MetaInfo {
 }
 
 pub fn get_system_meta_info() -> Vec<MetaInfo> {
-    let mut meta_infos: Vec<MetaInfo> = Vec::new();
-
     let mut s = System::new_all();
     s.refresh_all();
+    return get_meta_info_vec(s);
+}
 
-    let meta_info_num: HashMap<&'static str, (String, u64)> = {
-        let total_memory: u64 = s.total_memory();
-        let used_memory: u64 = s.used_memory();
-        let available_memory: u64 = s.available_memory();
-        let percentage_used_memory: u64 = (used_memory * 100) / total_memory;
+///
+/// Returns a HashMap of form {String, (String, u64)} with relevant meta information
+/// about the system running
+///
+fn get_meta_info_vec(s: System) -> Vec<MetaInfo> {
+    let mut meta_infos: Vec<MetaInfo> = Vec::new();
 
-        let total_swap: u64 = s.total_swap();
-        let used_swap: u64 = s.used_swap();
-        let free_swap: u64 = s.free_swap();
+    let total_memory: u64 = s.total_memory();
+    let used_memory: u64 = s.used_memory();
+    let available_memory: u64 = s.available_memory();
+    let percentage_used_memory: u64 = (used_memory * 100) / total_memory;
+    let total_swap: u64 = s.total_swap();
+    let used_swap: u64 = s.used_swap();
+    let free_swap: u64 = s.free_swap();
+    let num_cpus: u64 = s.cpus().len() as u64;
+    let system_name: Option<String> = System::name();
+    let system_kernel: Option<String> = System::kernel_version();
+    let os_version: Option<String> = System::os_version();
+    let host_name: Option<String> = System::host_name();
 
-        hashmap! {
-            "total_memory" => (convert_memory(total_memory), total_memory),
-            "used_memory" => (convert_memory(used_memory), used_memory),
-            "available_memory" => (convert_memory(available_memory), available_memory),
-            "percentage_used_memory" => (format!("{percentage_used_memory}%"), percentage_used_memory),
-            "free_swap" => (convert_memory(free_swap), free_swap),
-            "used_swap" => (convert_memory(used_swap), used_swap),
-            "total_swap" => (convert_memory(total_swap), total_swap)
-        }
-    };
+    meta_infos.push(create_meta_info_struct(
+        "total_memory".to_string(),
+        total_memory,
+        convert_memory(total_memory),
+    ));
 
-    for (name, val) in meta_info_num {
-        println!("{}: {}", name, val.0)
-    }
+    meta_infos.push(create_meta_info_struct(
+        "used_memory".to_string(),
+        used_memory,
+        convert_memory(used_memory),
+    ));
 
-    println!("System name: {:?}", System::name());
-    println!("System kernel version:   {:?}", System::kernel_version());
-    println!("System OS version:       {:?}", System::os_version());
-    println!("System host name:        {:?}", System::host_name());
-
-    println!("NB CPUs: {}", s.cpus().len());
+    meta_infos.push(create_meta_info_struct(
+        "available_memory".to_string(),
+        available_memory,
+        convert_memory(available_memory),
+    ));
+    meta_infos.push(create_meta_info_struct(
+        "used_swap".to_string(),
+        used_swap,
+        convert_memory(used_swap),
+    ));
+    meta_infos.push(create_meta_info_struct(
+        "total_swap".to_string(),
+        total_swap,
+        convert_memory(total_swap),
+    ));
 
     return meta_infos;
 }
 
+///
+/// Converts a byte value received as u64 into a string with an appropriate unit
+///
 fn convert_memory(value: u64) -> String {
     return Byte::from_u64(value)
         .get_appropriate_unit(UnitType::Binary)
         .to_string();
+}
+
+///
+/// Returns the value of an Option<String> as a string.
+/// If the option is empty an empty string gets returned.
+///
+fn get_string_from_option(option_string: Option<String>) -> String {
+    match option_string {
+        Some(name) => return name,
+        None => return "".to_string(),
+    }
+}
+
+fn create_meta_info_struct(name: String, value_i: u64, value_s: String) -> MetaInfo {
+    return MetaInfo {
+        name: name,
+        value_i: value_i,
+        value_s: value_s,
+    };
 }
