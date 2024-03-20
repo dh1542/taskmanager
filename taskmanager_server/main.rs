9bf5d123ad0e4e1bb1f2364d3d10bf7d34b61591
Dominik Hoffmann <dominik151099@outlook.de>
2024-03-20 21:31:03 +0100

    Managed to put first data to vue-app

--- taskmanager_server/main.rs
+++ taskmanager_server/main.rs
@@ -9,7 +9,7 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let routes = rest::routes::routes();
+    let routes = rest::routes::routes().with(warp::cors().allow_any_origin());
     println!("Server started at http://localhost:8000");
     warp::serve(routes).run(([127, 0, 0, 1], 8000)).await;
     let sys_meta: Vec<system::system_meta::MetaInfo> = system::system_meta::get_system_meta_info();
---
+++ taskmanager_ui/src/types/metaInfo.d.ts
@@ -0,0 +1,5 @@
+export type MetaInfo = {
+    name: string,
+    value_i: number,
+    value_s: string
+}
